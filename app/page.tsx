38d24bc2dac117e4717fc9ae96846516f10e9b92
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-11-12 15:24:11 -0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 // import from next
 // import Link from "next/link";
 // import components
-// import Background from "@/ui/background";
+// import Background from "./ui/background";
 // import PushNotificationSubscriptionManager from "./ui/push-notification-subscription-manager";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="text-center">
       {/* <Background /> */}
       {/* <PushNotificationSubscriptionManager /> */}
-      <h1>Hi Po!</h1>
+      <h1 className="text-2xl">Hi Po!</h1>
       <p>Not much to look at yet, but this is your new website in the making!</p>
     </div>
   );
