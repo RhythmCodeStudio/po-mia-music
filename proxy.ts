257a0c5252569950a8011dc691b7d673c8145263
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-11-11 19:17:27 -0600

    update

--- proxy.ts
+++ proxy.ts
@@ -11,13 +11,13 @@ export function proxy(request: NextRequest) {
     font-src 'self';
     style-src 'self' 'unsafe-inline';
     script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com;
-    connect-src 'self' https://api.emailjs.com/ https://vercel.live;
+    connect-src 'self' https://api.emailjs.com/ https://vercel.live https://vercel.com;
     img-src 'self' data: blob:;
     frame-src 'none';
   `;
   const previewCspHeader = `
     default-src 'self';
-    connect-src 'self' https://api.emailjs.com/ https://vercel.live;
+    connect-src 'self' https://api.emailjs.com/ https://vercel.live https://vercel.com;
     script-src 'self' 'nonce-${nonce}' https://vercel.live https://va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline' https://vercel.live;
     img-src 'self' data: blob: https://vercel.live https://vercel.com;
