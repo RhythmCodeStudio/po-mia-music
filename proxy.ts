34834907349be29fa5ab1a2eaaaa9ac25dd6d039
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-11-22 12:27:34 -0600

    update csp

--- proxy.ts
+++ proxy.ts
@@ -30,7 +30,7 @@ export function proxy(request: NextRequest) {
   const previewCspHeader = `
     default-src 'self';
     connect-src 'self' https://api.emailjs.com/ https://vercel.live https://vercel.com;
-    script-src 'self' 'nonce-${nonce}' https://vercel.live https://va.vercel-scripts.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' 'nonce-${nonce}' https://vercel.live https://va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline' https://vercel.live;
     img-src 'self' data: blob: https://vercel.live https://vercel.com;
     font-src 'self' https://vercel.live https://assets.vercel.com https://fonts.gstatic.com;
