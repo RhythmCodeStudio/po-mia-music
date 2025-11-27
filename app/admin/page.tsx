70cb2a19a8220b8fb58bfbd5f5eb20ff20d9bc7b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-11-26 21:03:56 -0600

    display number of subscribers in admin panel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,21 +1,25 @@
+// import data
+import { getSubscriptionsFromDB } from "@/lib/notification-subscriptions-db";
 // import from neon auth
 import { SignIn } from "@stackframe/stack";
 // import { AccountSettings } from "@stackframe/stack";
 // import components
 import AdminNotificationPanel from "@/app/ui/admin/admin-notification-panel";
-export default function AdminPage() {
+export default async function AdminPage() {
+  const subsctiptions = await getSubscriptionsFromDB();
+  const numberOfSubscriptions = subsctiptions.length;
   return (
     <div>
       {/* <div className="p-6">
         <h1 className="text-xl font-bold mb-6">Admin Dashboard</h1>
         <AdminNotificationPanel />
       </div> */}
-      <div>
+      <div className="flex justify-center">
         {/* <h1>Sign In</h1> */}
         {/* <SignIn
           fullPage={true}
         /> */}
-        <AdminNotificationPanel />
+        <AdminNotificationPanel  numberOfSubscriptions={numberOfSubscriptions} />
       </div>
     </div>
   );
--- app/ui/admin/admin-notification-panel.tsx
+++ app/ui/admin/admin-notification-panel.tsx
@@ -2,14 +2,14 @@
 import { useState } from "react";
 import { sendNotification } from "../../actions/actions";
 
-export default function AdminNotificationPanel() {
+export default function AdminNotificationPanel( {numberOfSubscriptions}: {numberOfSubscriptions?: number} ) {
+  console.log("numberOfSubscriptions in client:", numberOfSubscriptions);
   const [message, setMessage] = useState("");
   const [url, setUrl] = useState("");
   const [status, setStatus] = useState<null | string>(null);
 
   async function handleSend() {
     const result = await sendNotification(message, url);
-    console.log("sendNotification result:", result);
     setStatus(result && result.success ? "Notification sent!" : "Failed to send.");
     setMessage("");
     setUrl("");
@@ -17,7 +17,10 @@ export default function AdminNotificationPanel() {
 
   return (
     <div className="p-2">
-      <h2>Push Notification Admin</h2>
+      <h2>Welcome back Po.</h2>
+      <p>There are currently {numberOfSubscriptions ?? "Loading..."} people subscribed to notifications.</p>
+      <br />
+      <p>Send a new notification:</p>
       <input
         type="text"
         placeholder="Enter Notification Message"
@@ -25,6 +28,7 @@ export default function AdminNotificationPanel() {
         onChange={(e) => setMessage(e.target.value)}
         className="border p-1 rounded-md my-2 w-full max-w-md"
       />
+      <br />
       <input
         type="text"
         placeholder="Enter URL to open on click"
@@ -32,7 +36,8 @@ export default function AdminNotificationPanel() {
         onChange={(e) => setUrl(e.target.value)}
         className="border p-1 rounded-md my-2 w-full max-w-md"
       />
-      <button onClick={handleSend} className="mt-2">
+      <br />
+      <button onClick={handleSend} className="mt-2 px-4 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 border-1">
         Send Notification
       </button>
       {status && <p>{status}</p>}
