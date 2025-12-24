204105daad14df9628e49f3a1a6859fd71f702ca
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-23 20:15:22 -0600

    add bio to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import Toaster from "./ui/toaster";
 import InstallPrompt from "./ui/install-prompt";
+// import data
+import { poBio } from "../lib/po-data";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative flex flex-col flex-grow items-center justify-center space-y-12 ">
+      <h1 className="text-4xl lg:text-6xl font-bold pb-6 text-shadow-black-background-black">
+        po mia
+      </h1>
       <div className="fixed top-20 left-0 w-full z-50">
         <InstallPrompt />
       </div>
-      <div className="flex flex-col items-center w-full max-w-4xl mx-auto px-6 lg:px-0">
-        <h1 className="hidden">po mia</h1>
+      <div className="flex flex-col items-center w-full max-w-2xl mx-auto px-6 lg:px-0">
         <Image
           fetchPriority="high"
           src="/images/po-hearts.webp"
@@ -17,12 +21,17 @@ export default function Home() {
           width={2664}
           height={1902}
           priority
-          className="rounded-4xl shadow-white shadow-lg z-50"
+          className="rounded-4xl shadow-white shadow-lg z-10"
           style={{ border: "2px solid rgba(255, 255, 255, 0.3)" }}
         />
-        <Toaster />
       </div>
-    </>
+      <div className="bg-neutral-200/50 mt-6 py-4 rounded-4xl shadow-lg shadow-black/20 max-w-2xl mx-6 lg:mx-auto border-2 border-[rgba(255,255,255,0.3)]">
+        <p className="px-6 text-shadow-black-background-black w-full max-w-2xl mx-auto">
+          {poBio}
+        </p>
+      </div>
+      <Toaster />
+    </div>
   );
 }
 
