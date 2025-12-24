086055afd5223d2acacfc33fa69e19a045cd5f07
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-23 20:58:05 -0600

    add mailing list sign up from component

---
+++ app/ui/mailing-list-sign-up-form.tsx
@@ -0,0 +1,15 @@
+"use client";
+// import from react
+import { useState } from "react";
+// import components
+import ContactFormInput from "./contact-form-input";
+
+export default function MailingListSignUpForm() {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState("");
+
+  return (
+    <div></div>
+  );
+}
