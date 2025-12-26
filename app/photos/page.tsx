d72ababe1b86ddb578fab743aad9d7ffed4295cc
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-26 17:29:38 -0600

    add photo data

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,7 +1,9 @@
+
+
 export default function Photos() {
   return (
     <div>
-      <h1>Media Page</h1>
+      <h1>Photos</h1>
       <p>This is the photos page of the application.</p>
     </div>
   );
