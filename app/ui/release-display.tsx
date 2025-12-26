4dd43d6a23a15f56667ae9563900563d7f7d8cd2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-26 12:05:44 -0600

    add release display

---
+++ app/ui/release-display.tsx
@@ -0,0 +1,10 @@
+// import from next
+import Image from "next/image";
+
+export default function ReleaseDisplay() {
+  return (
+    <section>
+      Release Display Component
+    </section>
+  );
+}
