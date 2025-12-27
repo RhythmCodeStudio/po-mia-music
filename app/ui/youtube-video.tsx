1d19a8c92c5ea29e4c3c7395038ce692c1c38f94
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-26 19:15:57 -0600

    update

--- app/ui/youtube-video.tsx
+++ app/ui/youtube-video.tsx
@@ -31,7 +31,7 @@ export default function YouTubeVideo({ videoId, title, width = 560, height = 315
           height={height}
           src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} 
           title="YouTube video player"
-          frameBorder="0"
+          // frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
