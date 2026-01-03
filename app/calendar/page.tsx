bdad3fd7e581f14b4bddd91f88e44723ea159cc2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-01-03 10:32:52 -0600

    adjust spacing on calendar page

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -33,7 +33,7 @@ export default async function Calendar() {
         headingLevel={2}
         className="font-bold text-4xl text-shadow-black-background-black mt-6"
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 w-full max-w-6xl my-12 px-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 w-full max-w-6xl my-8 px-8">
         {upComingEvents.map((event) => (
           <CalendarEventDisplay
             id={event.id}
