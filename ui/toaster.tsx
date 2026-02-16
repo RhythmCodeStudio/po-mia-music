0f904ad32cec6ca6ee72d9c6be85421ca2fc4fa5
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-02-16 14:11:08 -0600

    use new toast content component

--- ui/toaster.tsx
+++ ui/toaster.tsx
@@ -6,35 +6,36 @@ import { ToastContainer, toast, Slide } from "react-toastify";
 //  import components
 import PushNotificationSubscriptionManager from "./push-notification-subscription-manager";
 import Button from "./button";
+import ToastContent from "./toast-content";
 // import context
 import { usePushNotification } from "../context/push-notification-context-provider";
 // import icon
 import { RiCloseCircleFill } from "react-icons/ri";
 
 // Toast content as a component to accept props from react-toastify
-function ToastContent() {
-  return (
-    <div>
-      <Button
-        onClick={() => toast.dismiss()}
-        className="absolute top-2 right-2 p-1"
-        aria-label="Close notification"
-        icon={<RiCloseCircleFill size={18} className="icon-shadow" />}
-      />
+// function ToastContent() {
+//   return (
+//     <div>
+//       <Button
+//         onClick={() => toast.dismiss()}
+//         className="absolute top-2 right-2 p-1"
+//         aria-label="Close notification"
+//         icon={<RiCloseCircleFill size={18} className="icon-shadow" />}
+//       />
 
-      <div className="mt-6 flex flex-col items-center text-center text-shadow-black-background-black rounded-[1rem]">
-        <p>
-          Subscribe to notifications to stay up to date with the latest from po
-          mia!
-        </p>
-        <div className="flex justify-center mx-auto">🎵✨🎵✨</div>
-        <div className="my-2">
-          <PushNotificationSubscriptionManager renderedAs="button" />
-        </div>
-      </div>
-    </div>
-  );
-}
+//       <div className="mt-6 flex flex-col items-center text-center text-shadow-black-background-black rounded-[1rem]">
+//         <p>
+//           Subscribe to notifications to stay up to date with the latest from po
+//           mia!
+//         </p>
+//         <div className="flex justify-center mx-auto">🎵✨🎵✨</div>
+//         <div className="my-2">
+//           <PushNotificationSubscriptionManager renderedAs="button" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 const toastMessage = () => {
   toast(<ToastContent />, {
