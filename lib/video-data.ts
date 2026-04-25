53df963a06f420952d85f597d014932216b59d8c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-04-25 16:34:14 -0500

    update videos

--- lib/video-data.ts
+++ lib/video-data.ts
@@ -1,27 +1,59 @@
 export const youTubeVideos = [
+  // {
+  //   youTubeId: "",
+  //   title: "",
+  //   description: "",
+  // },
+  {
+    youTubeId: "TuRwUk0eqAU",
+    title: "po mia @ The Garage STL",
+    description:
+      "On March 22nd Po Mia and their band performed at The Noticed Arts Expo ",
+  },
+  {
+    youTubeId: "GuGbM0EH27U",
+    title: "season the day (NPR Tiny Desk Submission 2026)",
+    description:
+      "po mia is an up-and-coming singer/songwriter, and this is their Tiny Desk Contest submission for 2026 !",
+  },
+  {
+    youTubeId: "0h143irxc_o",
+    title: "po mia — stormii seas (The Stage: Extended)",
+    description:
+      "po mia is taking St. Louis by storm, live on on KMOV 4's The Stage: Extended",
+  },
   {
     youTubeId: "ABO_nv26QEc",
     title: "po mia - exposé (My St. Louis Live!)",
-    description: "po mia's full band takes The Stage on KMOV 4's My St. Louis Live!",
+    description:
+      "po mia's full band takes The Stage on KMOV 4's My St. Louis Live!",
   },
   {
     youTubeId: "3-ZeWNYQ5x8",
     title: "po mia - Cyberchondria",
-    description: "Po Mia, an eclectic and colorful singer-songwriter based in St. Louis, delivered an unforgettable performance at Fortune Teller Bar. No stage, just raw, authentic music in an intimate setting.",
+    description:
+      "po Mia, an eclectic and colorful singer-songwriter based in St. Louis, delivered an unforgettable performance at Fortune Teller Bar. No stage, just raw, authentic music in an intimate setting.",
   },
   {
     youTubeId: "UbpvxgNmaKM",
-    title: "Po Mia Spreads Positivity Through Music",
-    description: "Po Mia is an eclectic local musician who aims to spread positivity and comfort with their music. Though they have been a musician for around a decade, they have only just begun playing live within the past few years and have already achieved local fame. Po's music is unique and complex, with vulnerable lyrics sung by a clear and melodic voice. With a background in music therapy, Po structures their music so that their audience will find a home in it, and be able to release negative emotions while listening to their songs. Audience connection and participation are instrumental in Po's live performances. Po's shows are also filled with surprises, and will occasionally include their party trick of rapping while solving a Rubix cube.",
-  },
-  {
-    youTubeId: "OrkQn-YwyR8",
-    title: "Po: Live from the Boom Room",
-    description: "Po: Live from the Boom Room",
+    title: "po mia Spreads Positivity Through Music",
+    description:
+      "po mia is an eclectic local musician who aims to spread positivity and comfort with their music. Though they have been a musician for around a decade, they have only just begun playing live within the past few years and have already achieved local fame. po's music is unique and complex, with vulnerable lyrics sung by a clear and melodic voice. With a background in music therapy, po structures their music so that their audience will find a home in it, and be able to release negative emotions while listening to their songs. Audience connection and participation are instrumental in po's live performances. po's shows are also filled with surprises, and will occasionally include their party trick of rapping while solving a Rubix cube.",
   },
   {
     youTubeId: "MQMXnzR575U",
-    title: "Season the Day - Po Mia",
-    description: "Po Mia performs Season the Day for Basement Sessions.",
+    title: "season the day - po mia",
+    description: "po mia performs Season the Day for Basement Sessions.",
+  },
+  {
+    youTubeId: "6dDGgF6kyXI",
+    title: "po mia - crazii (NPR Tiny Desk Submission 2023)",
+    description:
+      "this is po mia's NPR tiny desk submission for 2023, with their original song : crazii",
+  },
+  {
+    youTubeId: "OrkQn-YwyR8",
+    title: "po: Live from the Boom Room",
+    description: "po: Live from the Boom Room",
   },
 ];
