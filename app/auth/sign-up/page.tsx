b881046703ea2d65dd7a9a59951ddd44e83f450f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-04-27 13:37:19 -0500

    adjust spacing on sign in sign up pages

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -6,7 +6,7 @@ import SignUpForm from "@/ui/sign-up-form";
 export default function SignUpPage() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="relative">
+      <div className="my-6">
         <SignUpForm />
       </div>
     </div>
